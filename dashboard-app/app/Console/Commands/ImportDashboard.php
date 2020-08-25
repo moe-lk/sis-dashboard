8cfa3fa4383c70384c60a1d090b3159479b32409
Janith Silva <janithrs>
2020-08-25 13:10:24 +0530

    Implement import dahsboard feature

---
+++ dashboard-app/app/Console/Commands/ImportDashboard.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+
+class ImportDashboard extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dashboard:import {orgId} {filePath}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import Dashboards';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+
+    public function handle()
+    {
+        $arg1 = $this->argument('arg1');
+        $orgId = $this->argument('orgId');
+        try {
+            $header = [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ];
+            $client = new Client([
+                'base_uri' => env('GRAFANA_URL'),
+                'auth' => [env('GRAFANA_USER'), env('GRAFANA_PASSWORD')],
+                'headers' =>  $header
+            ]);
+            $response = $client->post('/api/dashboards/db', [
+                'json' => [
+                    "dashboard" => $arg1
+                    ,
+                    "folderId" => $orgId,
+                    "overwrite" => false
+                ]
+            ])->getBody();
+            $data = json_decode($response, true);
+            return $data;
+        } catch (\Throwable $th) {
+        }
+
+    }
+}
